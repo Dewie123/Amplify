1a4d7ee54d5701d28ea58d8c0d44f906c81d4c33
Dewie123 <infocommemailtaka@gmail.com>
2025-03-17 01:48:33 +0800

    x

--- fireJetReact/src/App.tsx
+++ fireJetReact/src/App.tsx
@@ -13,6 +13,7 @@ import BOTeamAvailabilityView from "./pages/BOTeamAvaibilityView";
 import BOReportIssuesViewSubmitAnIssue from "./pages/BOReportIssuesViewSubmitAnIssue";
 import BOReportIssuesView from "./pages/BOReportIssuesView";
 
+
 function App() {
   return (
     <Router>
@@ -38,10 +39,11 @@ function App() {
          <Route path="/bo-schedule-view" element={<BOScheduleView />} /> 
          {/* Route for Edit BOTeamAvailabilityView page */}
          <Route path="/bo-team-availability-view" element={<BOTeamAvailabilityView />} /> 
-         {/* Route for Edit BOReportIssuesView page */}
+         {/* Route for Edit BOReportIssuesFormView page */}
          <Route path="/bo-report-issues-form-view" element={<BOReportIssuesViewSubmitAnIssue />} /> 
          {/* Route for Edit BOReportIssuesView page */}
          <Route path="/bo-report-issues-view" element={<BOReportIssuesView />} /> 
+         
       </Routes>
     </Router>
   );
