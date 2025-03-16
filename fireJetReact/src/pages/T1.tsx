55b69ebf7afa9141ed00bcc89afa24d937a889f2
HASBULLA <chesterchua722@gmail.com>
2025-03-17 03:19:07 +0800

    Add files via upload

---
+++ fireJetReact/src/pages/T1.tsx
@@ -0,0 +1,22 @@
+import "./T1.css";
+
+export default function T({
+  className = "",
+  text = "Name",
+  text1 = "Marry Jane",
+}: TProps) {
+  return (
+    <div className={`${className} t-t-name-input-1`}>
+      <div className="t-name">{text}</div>
+      <div className="t-input-3">
+        <div className="t-marry-jane">{text1}</div>
+      </div>
+    </div>
+  );
+}
+
+interface TProps {
+  className?: string;
+  text: string;
+  text1: string;
+}
